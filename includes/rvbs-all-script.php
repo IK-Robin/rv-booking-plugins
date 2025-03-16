7341b93e494350972d7a9d69cb5e3388414c55b8
Robin <62151722+IK-Robin@users.noreply.github.com>
2025-03-16 12:29:11 +0600

    Add AJAX script for cart functionality and localize script parameters

--- includes/rvbs-all-script.php
+++ includes/rvbs-all-script.php
@@ -16,6 +16,22 @@ function rvbs_add_all_script() {
     ]);
 
 
+    // get the toplavel page and enqueue the script for only the book now page 
+    //add the add to chart js
+ 
+
+    wp_enqueue_script('rvbs-add-to-cart-js', plugin_dir_url(__FILE__) . '../assets/js/custom-add-to-cart-ajax.js', array('jquery'), RVBS_PLUGIN_VER, true);
+    //add the add to chart js
+
+
+    // localize the script 
+    wp_localize_script('rvbs-add-to-cart-js', 'rvbs_add_to_cart', array(
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'nonce' => wp_create_nonce('rvbs_add_to_cart_nonce'),
+        'add_to_cart' => 'add_to_cart',
+    ));
+
+
 }
 add_action('wp_enqueue_scripts', 'rvbs_add_all_script');
 
@@ -26,6 +42,9 @@ function enqueue_flatpickr() {
     wp_enqueue_script('flatpickr-js', 'https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.13/flatpickr.min.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_flatpickr');
+
+
+//
 
 
 ?>
