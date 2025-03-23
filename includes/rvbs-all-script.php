38ffff52fd783588f0f7762b402b2071c7d8b9bc
Robin <62151722+IK-Robin@users.noreply.github.com>
2025-03-23 11:36:06 +0600

    Add checkout JavaScript and localize script for AJAX functionality

--- includes/rvbs-all-script.php
+++ includes/rvbs-all-script.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 
 
-function rvbs_add_all_script() {
+function rvbs_add_all_script()
+{
 
     // add the jquery support 
     wp_enqueue_script('jquery');
@@ -10,7 +11,7 @@ function rvbs_add_all_script() {
     wp_enqueue_script('rvbs-bootstrap-js', plugin_dir_url(__FILE__) . '../assets/js/rvbs-bootstrap.min.js', array('jquery'), RVBS_PLUGIN_VER, true);
     wp_enqueue_script('rvbs-search-rv', plugin_dir_url(__FILE__) . '../assets/js/rvbs-search-rv.js', array('jquery'), RVBS_PLUGIN_VER, true);
 
-    wp_localize_script('rvbs-search-rv','rvbs_serch_rv',[
+    wp_localize_script('rvbs-search-rv', 'rvbs_serch_rv', [
         'ajax_url' => admin_url('admin-ajax.php'),
         'action' => 'load_more_posts'
     ]);
@@ -18,7 +19,7 @@ function rvbs_add_all_script() {
 
     // get the toplavel page and enqueue the script for only the book now page 
     //add the add to chart js
- 
+
 
     wp_enqueue_script('rvbs-add-to-cart-js', plugin_dir_url(__FILE__) . '../assets/js/custom-add-to-cart-ajax.js', array('jquery'), RVBS_PLUGIN_VER, true);
     //add the add to chart js
@@ -32,13 +33,22 @@ function rvbs_add_all_script() {
         'remove_from_cart' => 'remove_from_cart',
     ));
 
+    // add the checkout js 
 
+    
+        wp_enqueue_script('rvbs-checkout-js', plugin_dir_url(__FILE__) . '../assets/js/rvbs-checkout.js', array('jquery'), RVBS_PLUGIN_VER, true);
+        wp_localize_script('jquery', 'rvbs_checkout', array(
+            'ajax_url' => admin_url('admin-ajax.php'),
+            'nonce' => wp_create_nonce('rvbs_checkout_nonce')
+        ));
+    
 }
 add_action('wp_enqueue_scripts', 'rvbs_add_all_script');
 
 
 
-function enqueue_flatpickr() {
+function enqueue_flatpickr()
+{
     wp_enqueue_style('flatpickr-css', 'https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.13/flatpickr.min.css');
     wp_enqueue_script('flatpickr-js', 'https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.13/flatpickr.min.js', array(), null, true);
 }
@@ -46,6 +56,3 @@ add_action('wp_enqueue_scripts', 'enqueue_flatpickr');
 
 
 //
-
-
-?>
