47c23e909ca1c35138e3675f4ed3998405eee567
Robin <62151722+IK-Robin@users.noreply.github.com>
2025-03-23 12:48:14 +0600

    Add filter action for RV lot search and enqueue flatpickr script

--- includes/rvbs-all-script.php
+++ includes/rvbs-all-script.php
@@ -10,10 +10,12 @@ function rvbs_add_all_script()
     // 
     wp_enqueue_script('rvbs-bootstrap-js', plugin_dir_url(__FILE__) . '../assets/js/rvbs-bootstrap.min.js', array('jquery'), RVBS_PLUGIN_VER, true);
     wp_enqueue_script('rvbs-search-rv', plugin_dir_url(__FILE__) . '../assets/js/rvbs-search-rv.js', array('jquery'), RVBS_PLUGIN_VER, true);
-
+    // enqueue_flatpickr() js 
+    // wp_enqueue_script('flatpickr', plugin_dir_url(__FILE__) . '../assets/js/rvbs-flatpicke.min.js', array('jquery'), RVBS_PLUGIN_VER, true);
     wp_localize_script('rvbs-search-rv', 'rvbs_serch_rv', [
         'ajax_url' => admin_url('admin-ajax.php'),
-        'action' => 'load_more_posts'
+        'action' => 'load_more_posts',
+        'filter_rv_lots' => 'filter_rv_lots'
     ]);
 
 
